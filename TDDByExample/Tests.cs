ad344bd5f59c11bc4a094baa6a38c6d150cb9281
ivans <ivans@gmail.com>
2019-03-18 22:47:57 +0000

    first commit

---
+++ TDDByExample/Tests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+/**
+ * Instrument   Shares  Price   Total
+ * IBM          1000    25USD   25000USD
+ * GE           400     150CHF  40000CHF
+ *                              65000USD
+ * 
+ * From         To      Rate
+ * CHF          USD     1.5
+ * 
+ */
+
+namespace TDDByExample
+{
+    class Tests
+    {
+        [Test]
+        public void Test_Multiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.times(2);
+            Assert.That(10, Is.EqualTo(five.amount));
+        }
+    }
+
+    class Dollar
+    {
+        public int amount { get; set; }
+
+        public Dollar(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public void times(int times)
+        {
+            this.amount *= times;
+        }
+    }
+}
