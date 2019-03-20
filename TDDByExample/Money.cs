dab1e2bcb35664ce5e4b50863ab8e23b09db0aa5
ivans <ivans@gmail.com>
2019-03-20 00:32:15 +0000

    finished chapter 6

---
+++ TDDByExample/Money.cs
@@ -0,0 +1,18 @@
+﻿/**
+ * Instrument   Shares  Price   Total
+ * IBM          1000    25USD   25000USD
+ * GE           400     150CHF  40000CHF
+ *                              65000USD
+ * 
+ * From         To      Rate
+ * CHF          USD     1.5
+ * 
+ */
+
+namespace TDDByExample
+{
+    class Money
+    {
+        protected int amount { get; set; }
+    }
+}
